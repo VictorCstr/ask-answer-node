cc09b3e3262e282450a0cc40892f18a5ac738062
VictorCstr <victorcastro.dev@hotmail.com>
2022-03-15 19:37:06 -0300

    editada porta para heroku

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(routes);
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('Servidor rodando na porta ' + port);
 })
